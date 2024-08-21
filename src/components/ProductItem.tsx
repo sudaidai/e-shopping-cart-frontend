de9b0a9d4d55c035ec65c0226bbfc42e4271e066
sudaidai <57120082+sudaidai@users.noreply.github.com>
2024-08-21 22:31:32 +0800

    animation wahaha

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import './product-item.css';
 
 interface Product {
   id: string;
@@ -14,22 +18,21 @@ interface ProductItemProps {
 }
 
 const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
-
   return (
     <Link href={{ pathname: `/product/${product.id}` }}>
-        <Image
-          src={`/product/${product.id}.jpg`}
-          alt="Product Image"
-          className="w-[270px] h-[230px] cursor-pointer"
-          width={270}
-          height={230}
-        />
-        <div className="flex flex-col items-center relative mt-2">
-            <div className="opacity-100 transition-opacity duration-500 ease">
-              <div className="text-lg font-bold my-2">{product.name}</div>
-              <div className="text-center">{`Price: ${product.price} NTD`}</div>
-            </div>
+      <Image
+        src={`/product/${product.id}.jpg`}
+        alt="Product Image"
+        className="w-[270px] h-[230px] cursor-pointer transition-transform duration-150 ease-out hover:scale-105 hover:ease-in"
+        width={270}
+        height={230}
+      />
+      <div className="flex flex-col items-center relative mt-2">
+        <div className="fade-in">
+          <div className="text-lg font-bold my-2">{product.name}</div>
+          <div className="text-center">{`Price: ${product.price} NTD`}</div>
         </div>
+      </div>
     </Link>
   );
 };
